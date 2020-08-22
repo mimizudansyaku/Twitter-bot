68e8514bc5fb06d6045060b0672a77edac94d510
mimizudansyaku <sknm2525@docomo.ne.jp>
2020-08-22 12:12:10 +0900

    connect database

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 var express = require('express')
 var Twitter = require('twitter')
 var CronJob = require("cron").CronJob
-// var dotenv = require('dotenv')
-
+var mysql = require('mysql')
 
 var app = express()
-// dotenv.config()
+
+
 
 // ツイッターのキーとシークレットトークンを初期化（環境変数を使用）
 var twitter = new Twitter({
@@ -29,40 +29,58 @@ new CronJob({
   start: true
 })
 
+/*
+// MySQL in Appデータベース接続詞
+var connectionString = process.env.MYSQLCONNSTR_localdb;
+var host = /Data Source=([0-9\.]+)\:[0-9]+\;/g.exec(connectionString)[1];
+var port = /Data Source=[0-9\.]+\:([0-9]+)\;/g.exec(connectionString)[1];
+var database = /Database=([0-9a-zA-Z]+)\;/g.exec(connectionString)[1];
+var username = /User Id=([a-zA-z0-9\s]+)\;/g.exec(connectionString)[1];
+var password = /Password=(.*)/g.exec(connectionString)[1];
+var exampleSql = "";
 
-// 呟く内容の配列
-var tipsArray = [
-  "朝食べるたんぱく質がトリプトファンになり、昼間にセロトニンになり、夜メラトニンになります。朝食に納豆味噌汁焼き魚は理にかなっているわけです",
-  "ふつうに食事をとっていても女性は鉄分が不足しがちです。サプリメントで補いましょう",
-  "1日に3食取るようになったのは江戸時代の元禄年間からです。照明器具の発達で夜がまで活動できるようになったからです。それまでは2食が普通でした",
-  "2016年カナダとオーストラリアの共同研究で、アルコールの腸内環境や脳への悪影響が立証されました。酒は百薬の長論は完全否定されました",
-  "人類は遠い昔にビタミンCを生成する能力を失いました。現代人はさらにビタミンB3（ナイアシン）の生成能力を失いつつあります",
-  "生野菜を食べるとき、葉野菜は洗いましょう。農薬だけでなく軟体動物の這った痕などさまざまな因子がついています。外食チェーンの生サラダでは洗っていないでしょう",
-  "現代医学は症状を悪としてそれに対抗するように薬を次々と開発してきました。特に国民皆保険の日本人は簡単に薬を欲しがります。結果、本来の免疫力は弱ってしまったといえます",
-  "スーパーの刺身売り場で、ブロックでなく切り身で売っているものは相当量のエタノールをふりかけて切っています",
-  "むかしは二人にひとりもの割合で癌になったでしょうか。アルツハイマーや難病患者がますます増えています。死ぬ病は減りましたが死ねない病が増えました。医学は発達しているのでしょうか",
-  "お酒を控える基準に誰もがγ-GPTを気にしています。しかし本当に悪影響があるのは脳に対してです。健康診断ではわかりません",
-  "精白した米や小麦にはほとんど栄養素が入っていません。医師の玄米主食率が高いのは本当です",
-  "ノンカロリー、ノンシュガーと謳っている製品は本当に良いのでしょうか。それらの原材料名一覧をご覧ください。代わりにアセスルファムK、アスパルテームなどの人工甘味料が入っています",
-  "食事、運動は自分でコントロールできますが睡眠はそうもいかない場合があります。睡眠薬には依存性の重篤なものが多いので、医師の処方を盲信せず自分で調べることが大切です",
-  "精神的不調のほとんどは休息と食事療法で治癒できます。基本は高たんぱく低糖質です。女性は鉄分も。ちなみに医大では食事療法について学びません",
-  "ランニング等有酸素運動はとても良い運動です。しかし運動したあとにビール、は美味しいですが、脱水状態のうえにさらに脱水効果の大きな飲料物を摂るわけですからカラダには危険行為です",
-  "お酒もタバコも（ただし添加物無添加のもの）、違法ですが大麻ですら、時々嗜む程度なら問題ないのです。毎日摂取することが問題なのです。「合法だから問題ない」わけではないです"
-]
+var connection = mysql.createConnection({
+  host     : host,
+  port     : port,
+  user     : username,
+  password : password,
+  database : database,
+  debug    : true
+});
+*/
 
+var conn = mysql.createConnection({
+  host: "database-testapp.mysql.database.azure.com",
+  user: "mimizudansyaku@database-testapp",
+  password: {your_password},
+  database: {your_database},
+  port: 3306,
+  ssl:{ca:fs.readFileSync({ca-cert filename})}
+});
 
 
 
 // ランダムにつぶやく関数
 function cycleTweet() {
-  var tips = tipsArray[Math.floor(Math.random() * tipsArray.length)]
 
-  // 自動投稿
-  twitter.post('statuses/update', {status: tips}, (err, tweet, response)=> {
+  //tipsテーブルからランダムにとってくるselect文
+  connection.query('select tips from tips order by rand() limit 1', function(err, rows) {
     if(err) {
       return console.log(err)
     }else{
-      return console.log(tweet)
+      tips = rows[0].tips
+      console.log(tips)
+
+
+      // 自動投稿
+      twitter.post('statuses/update', {status: tips}, (err, tweet, response)=> {
+        if(err) {
+          return console.log(err)
+        }else{
+          return console.log(tweet)
+        }
+      })
+
     }
   })
 }
